2949d6bfb3d9eb8087a17c49a971538c1a8aa349
Ioana Porsega <iporsega87@gmail.com>
2022-01-10 13:40:15 +0200

    Homework 8 - Better but still incomplete

--- homework8 - comment-box/app.js
+++ homework8 - comment-box/app.js
@@ -1,7 +1,5 @@
 // rating stars
 
-// var ratingStars = document.querySelector("#rating__star");
-
 var ratingStars = [...document.getElementsByClassName("rating__star")];
 
 function executeRating(stars) {
@@ -28,7 +26,19 @@ executeRating(ratingStars);
 var inputFullName = document.querySelector("#name");
 var inputCountry = document.querySelector("#country");
 var inputMessage = document.querySelector("#message");
-var img = "https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/:/128"; 
+
+// create the array for the messages:
+
+function UserMessage(img, rating, fullName, country, message) {
+    this.rating = rating;
+    this.img = img;
+    this.fullName = fullName;
+    this.country = country;
+    this.message = message;
+    this.showInfo = function() {
+        return "<img src='" + this.img + "'>" + "<br>" + this.rating + "<br>" + "<h5>" + this.fullName + "</h5>" + "<small>" + this.country + "</small>" + "<p>" + this.message + "</p>" + "<button class='remove-comment'>Remove Comment</button>";
+    }
+}
 
 // create a main div for all the commments:
 
@@ -39,21 +49,6 @@ var userComment = document.createElement("div");
 userComment.classList.add("user-comment");
 allMsg.appendChild(userComment);
 
-// create the array for the messages:
-
-var messages = [];
-
-function UserMessage(img, fullName, country, message) {
-    this.img = img;
-    this.fullName = fullName;
-    this.country = country;
-    this.message = message;
-    this.showInfo = function() {
-        // return this.img + this.fullName + this.country + this.message;
-        return this.img + "<br>" + this.fullName + "<br>" + this.country + "<br>" + this.message;
-    }
-}
-
 // call the function when the button is clicked:
 
 var createCommentBtn = document.querySelector("button");
@@ -61,25 +56,28 @@ createCommentBtn.addEventListener("click", addComment);
 
 // Go through the array to display the comments in the main div:
 
+var messages = [];
+var output = "";
+
 function showInfo () {
-    var output = "";
-    for(var i = 0; i < messages.length; i++) {
+    for(var i = 0; i < messages.length; i++) {  
         output +=  messages[i].showInfo() + "<br>";
     }
     userComment.innerHTML = output;
 }
 
 function addComment() {
-    var message = new UserMessage (
-        "https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/:/128",
-        inputFullName.value,
-        inputCountry.value,
-        inputMessage.innerHTML
-        );
-        console.log(inputMessage.innerHTML);
-        messages.push(message);
-        showInfo();
-    }
+    var message = new UserMessage ("https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/:/128", ratingStars.value, inputFullName.value, inputCountry.value, inputMessage.value);
+    messages.push(message);
+    showInfo();
+}
+
+// remove current comment
     
-    
-    
+var removeComm = document.createElement("button");
+removeComm.classList.add("remove-comment");
+removeComm.innerHTML = "Remove Comment";
+removeComm.addEventListener("click", removeSingleComment);
+function removeSingleComment() {
+}
+
